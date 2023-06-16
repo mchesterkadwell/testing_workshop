8838b119d53eb7a7e587dd9133a79705deaae6ba
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2023-06-16 15:26:59 +0100

    Initial commit of project generated by Cookiecutter

---
+++ testing_workshop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from testing_workshop.users.models import User
+from testing_workshop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

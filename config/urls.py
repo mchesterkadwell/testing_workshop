3d7b871f94d68541231e1049b12bd2090f77d90c
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2023-06-21 19:10:41 +0100

    Remove unneeded About page.

--- config/urls.py
+++ config/urls.py
@@ -5,15 +5,17 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
+from testing_workshop import views
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("testing_workshop.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("ner/", views.ner, name="ner"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
